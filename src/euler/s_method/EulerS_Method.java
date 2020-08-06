b930de9ad9b808083534e7982a91e416c77d1e01
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2020-08-07 05:33:57 +0600

    first commit

---
+++ src/euler/s_method/EulerS_Method.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euler.s_method;
+
+/**
+ *
+ * @author Tahsina Keya
+ */
+public class EulerS_Method {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
